229e9386b7f2ae3f39fc6d8c3686d178286a9c3d
Antonio Vitor Nascimento de Souza <20221064010030@ifrn.local>
2025-05-14 11:42:24 -0300

    primeira parte da atividade

--- app/controllers/Postagens.java
+++ app/controllers/Postagens.java
@@ -1,5 +1,22 @@
 package controllers;
 
-public class Postagens {
+import java.util.List;
 
+import models.Postagem;
+import play.mvc.Controller;
+
+public class Postagens extends Controller{
+
+	public static void listar() {
+		List<Postagem> listpost = Postagem.findAll();
+		render();
+	}
+	
+	public static void salvar(Postagem p) {
+		p.save();
+		listar();
+	}
+	 public void post() {
+		 render();
+	 }
 }
--- app/models/Postagem.java
+++ app/models/Postagem.java
@@ -1,5 +1,17 @@
 package models;
 
-public class Postagem {
+import java.sql.Date;
 
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Postagem extends Model{
+	
+	public String titulo;
+	public String conteudo;
+	public String autor;
+	public Date data;
+	
 }
